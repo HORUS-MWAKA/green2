70d4f8c47468aab1c715acffb3f01f5d9aee29d3
HORUS-MWAKA <77971510+HORUS-MWAKA@users.noreply.github.com>
2024-06-03 18:28:20 +0600

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -63,8 +63,8 @@ if (selected == 'Watering Prediction'):
     
     if st.button('Watering Test Result'):
         st.write(Temperature,Humidity,Moisture,Watering)
-        diab_prediction = logic_watering_model.predict([[Temperature, Humidity, Moisture, Watering]])
-        #diab_prediction = logic_aeration_model.predict([[33,67,25,0]]) 
+        #diab_prediction = logic_watering_model.predict([[Temperature, Humidity, Moisture, Watering]])
+        diab_prediction = logic_aeration_model.predict([[33,67,25,0]]) 
         st.write(diab_prediction)
         #if (diab_prediction[0] == 1):
           #diab_diagnosis = 'Aeration Needed'
