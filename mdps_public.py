ee4acca96bff7199053dfcf3fe5307aa2f03c772
HORUS-MWAKA <77971510+HORUS-MWAKA@users.noreply.github.com>
2024-06-03 17:41:29 +0600

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -102,7 +102,8 @@ if (selected == 'Aeration Prediction'):
     # creating a button for Prediction
     
     if st.button('Aeration Test Result'):
-        heart_prediction = logic_aeration_model.predict([[Temperature, Humidity, Moisture, Aeration]])                          
+        #heart_prediction = logic_aeration_model.predict([[Temperature, Humidity, Moisture, Aeration]])     
+        heart_prediction = logic_aeration_model.predict([[33,67,25,0]) 
         
         if (heart_prediction[0] == 1):
           heart_diagnosis = 'Aeration Needed'
