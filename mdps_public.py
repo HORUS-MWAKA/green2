8768f2596242213962f0d77c18d530589a78f479
HORUS-MWAKA <77971510+HORUS-MWAKA@users.noreply.github.com>
2024-06-03 17:16:29 +0600

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,8 +12,8 @@ from streamlit_option_menu import option_menu
 
 
 # loading the saved models
-logic_watering_model = joblib.load('magic_joblib.sav')
-#logic_watering_model = pickle.load(open('logic_watering_model1.sav', 'rb'))
+#logic_watering_model = joblib.load('magic_joblib.sav')
+logic_watering_model = pickle.load(open('logic_aeration_model.sav', 'rb'))
 
 logic_aeration_model = pickle.load(open('logic_aeration_model.sav', 'rb'))
 
